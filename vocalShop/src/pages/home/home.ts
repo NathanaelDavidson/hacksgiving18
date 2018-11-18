be316fc5d3a78ad97c0a2f520cdc5a00cc5d22d3
Andrei <andy.karavanov@gmail.com>
2018-11-17 18:23:37 -0800

    -Added DialogFlow

--- vocalShop/src/pages/home/home.ts
+++ vocalShop/src/pages/home/home.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import { SpeechRecognition } from '@ionic-native/speech-recognition';
-import {  NgZone } from '@angular/core';
+
+declare var ApiAIPromises: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  answer;
 
   isListening: boolean = false;
   matches: Array<String>;
 
-  constructor(public navCtrl: NavController, public speech: SpeechRecognition, private zone: NgZone) {
+  constructor(public navCtrl: NavController, public speech: SpeechRecognition, public platform: Platform, public ngZone: NgZone) {
+    platform.ready().then(() => {
+      ApiAIPromises.init({
+        clientAccessToken: "72006c4653e74f64a37a74aaa8fac592"
+      }).then(result => console.log(result));
+    });
   }
 
   async hasPermission():Promise<boolean> {
@@ -47,11 +55,22 @@ export class HomePage {
 
     this.speech.startListening()
       .subscribe(matches => {
-        _this.zone.run(() => {
+        _this.ngZone.run(() => {
           _this.matches = matches;
         })
       }, error => console.error(error));
 
+  }
+
+  ask(question) {
+    ApiAIPromises.requestText({
+      query: question
+    })
+    .then(({result: {fulfillment: {speech}}}) => {
+       this.ngZone.run(()=> {
+         this.answer = speech;
+       });
+    })
   }
 
   toggleListenMode():void {
